01b081c53fa94078b465549e5640da53077b546a
Student <Student>
2016-03-07 15:37:00 +0100

    particle-update

--- cubegame.js
+++ cubegame.js
@@ -3,14 +3,10 @@ var app = angular.module("MyApp", []);
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-
 var gravity = 0.5;
 var angle = 0;
 
 var player = { x: c.width / 2, y: 50, width: 50, height: 50, velY: 0, speed: 10, jumping: true, jumpcycle:false};
-
-var particles = new Array();
-particlelife = 50;
 
 
 (function () {
@@ -33,153 +29,136 @@ app.controller("cubegame", function ($scope) {
         }
     }
 
-    // background
 
-    $scope.drawBG = function () {
+    // PARTICLE
+    var particles = [];
 
-    };
-
-    // forground
-
-    $scope.drawPlayer = function () {
-      ctx.fillStyle = "#FF0000";
-      ctx.fillRect(-(player.width / 2), -(player.height / 2), player.width, player.height);
-      ctx.stroke();
-    };
-
-    $scope.drawFG = function () {
-        $scope.drawPlayer();
-    };
-
-    function particle(x,y,vel,life,size,totalvel){
+    function Particle(x, y, vel, size) {
         this.x = x;
         this.y = y;
         this.vel = vel;
-        this.life = life;
         this.size = size;
-        this.totalvel = totalvel;
-    };
 
-
-    var particles = [];
-    var particlecontainer = [];
-    var maxParticles = 20;
-
-    function addparticles(){
-  //  if (particles.length > maxParticles) return;
-
-      for (var i = 0;i < 10; i++){
-        particles.push(new particle);
-        particles[i].x= player.x + (Math.random() * 5) + 1;
-        particles[i].y= player.y+(player.height/2) - (Math.random() * 10);
-        particles[i].vel = (Math.random() * 0.5) + 1;
-        particles[i].totalvel = particles[i].vel;
-        particles[i].size = (Math.random() * 20) + 10;
-      }
-        console.log(particles);
-        console.log(particles[0])
-    };
-
-
-    function drawparticles(){
-     
-       
-        for (var i = 0; i < particles.length; i++) {
-            var x = particles[i].x;
-            var y = particles[i].y;
-            var size = particles[i].size;
-            var velocity = particles[i].totalvel;
-
+        this.update = function () {
             ctx.save();
-            ctx.translate(x-=velocity, y);
-            ctx.fillStyle = "#b83bff";
-            ctx.fillRect(-size/2, -size/2, size, size);
+            ctx.translate(x, y);
+           // ctx.fillStyle = "#b83bff";
+            if (player.jumping == false) {
+                ctx.fillStyle = "#b83bff";
+            } else {
+                ctx.fillStyle = "#ff9900";
+            }
+
+            ctx.fillRect(-size / 2, -size / 2, size, size);
             ctx.restore();
-         
-            particles[i].totalvel += particles[i].vel;
-            particles[i].size /= 1.04;
+
+            x -= vel
+            if (size > 0) {
+                size -= 1;
+            };
+        };
+    };
+
+    function ParticleUpdate() {
+        if (particles.length <= 30) {
+            particles.push(new Particle(player.x, player.y + (Math.random() * 20), 5, 20));
+        };
+
+        if (particles.length > 25) {
+            particles.splice(0, 5)
+        };
+
+        for (var i = 0; i < particles.length; i++) {
+            particles[i].update();
         }
+    };
 
-        particlelife += 1;
-      };
+    //BOXES
+    function Box(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = 20;
+    };
 
+    var boxes = [];
+    function BoxUpdate() {
 
-    function update() {
+    };
 
+    //PLAYER
+    function PlayerUpdate() { 
         player.velY += gravity;
         player.y += player.velY;
 
         if (player.y >= c.height - player.height) {
             player.y = c.height - player.height;
             player.jumping = false;
-          //  angle = 0;
-           
-       }
-
-        ctx.clearRect(0, 0, c.width, c.height);
+        }
 
         ctx.save();
         ctx.translate(player.x, player.y);
         ctx.rotate(angle * Math.PI / 180);
-
-
+        
         if (player.jumping == true && player.jumpcycle == true) {
             angle += 10;
         }
+
         if (angle >= 360) {
             angle = 0;
             player.jumpcycle = false;
         };
-            
-       $scope.drawPlayer();
-         
-       ctx.restore();
 
-       drawparticles();
+        ctx.fillStyle = "#FF0000";
+        ctx.stroke();
+        ctx.fillRect(-(player.width / 2), -(player.height / 2), player.width, player.height);
 
-       // <<<<
-
-    if(player.jumping == false){
-        if (particlelife > 50){
-            // addparticles();
-            particlecontainer.push(new addparticles);
-            console.log(particlecontainer);
-            particlelife = 0;
-        }
-
-    }
-    
-
-        requestAnimationFrame(update);
+        ctx.restore();
     };
 
-    window.addEventListener("load", function () {
-      // particles.push(new singleparticle(500,500+Math.random() * 30) + 1);
-       // particles.push(new singleparticle(500,500+Math.random() * 30) + 1);
-       // console.log(particles[0]);
-        update();
-      //  addparticles();
-  
+    // background
+    function bgUpdate() {
+
+    };
+
+    // forground
+    function fgUpdate() {
+        ParticleUpdate();
+        PlayerUpdate();
+    };
+
+    function Update() {
+        ctx.clearRect(0, 0, c.width, c.height);
+
+        bgUpdate();
+        fgUpdate();
+     
+        requestAnimationFrame(Update);
+    };
+
+window.addEventListener("load", function () {
+ Update();
+});
+ 
 
 // TEST UPDATE FUNCTION ---------
     function update2() {
-            ctx.clearRect(0, 0, 1280, 720);
+        ctx.clearRect(0, 0, 1280, 720);
 
-            angle += 0.01;
-           // incrementAngle();
-            ctx.save();
-            ctx.lineWidth = 10;
-            ctx.translate(200, 200);
-            ctx.rotate(angle);
+        angle += 0.01;
+        // incrementAngle();
+        ctx.save();
+        ctx.lineWidth = 10;
+        ctx.translate(200, 200);
+        ctx.rotate(angle);
 
-            ctx.fillStyle = "#FF0000";
-            ctx.fillRect(-25, -25, 50, 50);
-            ctx.strokeRect(-25, -25, 50, 50);
-            ctx.restore();
-            ctx.translate(0.5, 0)
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(-25, -25, 50, 50);
+        ctx.strokeRect(-25, -25, 50, 50);
+        ctx.restore();
+        ctx.translate(0.5, 0)
             
-           requestAnimationFrame(update2);
-        }
-
-    });
+        requestAnimationFrame(update2);
+    }
 });
+
+
